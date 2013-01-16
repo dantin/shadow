b1c57c2030974768a6ffa7d0fdef5f836d7e1bad
dantin <chengjie.ding@gmail.com>
2013-01-16 15:23:06 +0800

    add hello.c, print the process ID

---
+++ c/apue/file/hello.c
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) APUE, all rights reserved.
+ *
+ * filename    : hello.c
+ * version     : 1.0
+ * author      : david
+ * date        : 2013-01-16
+ * description : Print the process ID
+ *
+ */
+#include "ourhdr.h"
+
+int main(void)
+{
+  printf("Hello world from process ID %d\n", getpid());
+
+  exit(0);
+}
