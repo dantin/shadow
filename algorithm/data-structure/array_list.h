75ca2abe8341feeec2a4533424fc957ce71631b8
David Ding <chengjie.ding@gmail.com>
2013-08-25 11:29:34 +0800

    refactoring array list api

--- algorithm/data-structure/array_list.h
+++ algorithm/data-structure/array_list.h
@@ -26,32 +26,40 @@ typedef struct {
   long limit;
 } ArrayList;
 
+typedef bool Status;
+
 /*
- * 构造一个空的线性表
+ * 新建顺序线性表节点，并由node返回
+ *
+ * node   返回的新建节点
+ * data   实际数据
+ */
+Status make_list_node( ArrayListNode **node, void *data );
+
+/*
+ * 释放顺序线性表节点
+ *
+ * node   待释放的节点地址
+ */
+void destroy_list_node( ArrayListNode **node );
+
+/*
+ * 构造一个空的顺序线性表
  *
  * list   目标线性表指针
  */
-void init_list( ArrayList *list );
+Status init_list( ArrayList *list );
 
 /*
- * 销毁线性表，调用后list不能再被使用
+ * 将顺序线性表重置为空表，并释放原链表节点空间
  *
  * list   目标线性表指针
  * clear  清理函数，用户提供
  */
-
-void destroy_list( ArrayList *list, void (*clear)(void *) );
-
-/*
- * 重置线性表，将线性表重置为空表
- *
- * list   目标线性表指针
- * clear  清理函数，用户提供
- */
-void clear_list( ArrayList *list, void (*clear)(void *) );
+Status clear_list( ArrayList *list, Status ( *clear )( void * ) );
 
 /*
- * 判断线性表是否为空表
+ * 判断顺序线性表是否为空表
  *
  *  空表返回TRUE
  *  否则返回FALSE
@@ -61,84 +69,146 @@ void clear_list( ArrayList *list, void (*clear)(void *) );
 bool is_empty_list( ArrayList *list );
 
 /*
- * 线性表大小
+ * 获取顺序线性表大小
  *
- *  返回线性表中元素个数
+ * 返回顺序线性表中元素个数
  *
  * list   目标线性表指针
  */
 long list_size( ArrayList *list );
 
 /*
- * 获取线性表中某位置的数据
+ * 获取顺序线性表中某位置的节点地址
  *
- *  返回该位置的元素，若找不到，返回空
+ * 找到返回TRUE，若找不到，返回FALSE
  *
  * list   目标线性表指针
- * index  元素下标, 定义域为 [0, list_size( list )]
+ * index  元素下标
+ * node   返回的目标节点地址
  */
-void * get_list_element( ArrayList *list, long index );
+Status locate_list_node_by_position( ArrayList *list, long index, ArrayListNode **node );
 
 /*
- * 搜索线性表中与key满足compare关系的的第一个数据元素位置
+ * 搜索顺序线性表中与key满足compare关系的的第一个数据节点地址
  *
- *  返回与key满足compare关系的第一个元素的位置
- *  找不到返回-1
+ * 返回与key满足compare关系的第一个数据节点地址
+ * 找不到返回NULL
  *
  * list      目标线性表指针
  * key       目标元素
  * compare   函数指针，定义目标元素和数据元素之间关系
  */
-long locate_list_element( ArrayList *list, void *key, int (*compare)(const void *, const void *) );
+ArrayListNode *locate_list_node_by_locator( ArrayList *list, void *key, int (*compare)(const void *, const void *) );
 
 /*
- * 搜索线性表中前驱节点
+ * 搜索顺序线性表中目标节点的前驱节点地址
  *
- *   返回前驱节点数据
- *   无前驱节点返回空
- *
- * list       目标线性表指针
- * location   元素下标, 定义域为 [1, list_size( list )]
+ * list   目标线性表指针
+ * pos    目标节点
  */
-void * get_previous_element( ArrayList *list, long location );
+ArrayListNode *get_previous_node( ArrayList *list, ArrayListNode *pos );
 
 /*
- * 搜索线性表中的后继节点
+ * 搜索顺序线性表中目标节点的后继节点地址
  *
- *   返回后继结点数据
- *   无后继结点返回空
- *
- * list       目标线性表指针
- * location   元素下标，定义域为[0, list_size( list ) - 1 ]
+ * list   目标线性表指针
+ * pos    目标节点
  */
-void * get_next_element( ArrayList *list, long location );
+ArrayListNode * get_next_node( ArrayList *list, ArrayListNode *pos );
 
 /*
- * 在index前插入一个节点
+ * 将节点插入顺序线性表表头
+ *
+ * list      目标线性表指针
+ * node      待插节点指针
+ */
+Status insert_list_head( ArrayList *list, ArrayListNode *node );
+
+/*
+ * 将节点插入顺序线性表表尾
+ *
+ * list      目标线性表指针
+ * node      待插节点指针
+ */
+Status append_list_tail( ArrayList *list, ArrayListNode *node );
+
+/*
+ * 将节点插入顺序线性表中某一位置之前
  *
  * list    目标线性表指针
- * data    元素数据指针
- * index   元素下标，定义域为[0, list_size( list )]
+ * data    目标插入位置
+ * index   目标节点
  */
-void list_insert( ArrayList *list, void *data, long index );
+Status insert_before_list_node( ArrayList *list, ArrayListNode **pos, ArrayListNode *node );
+
+/*
+ * 将节点插入顺序线性表中某一位置之后
+ *
+ * list    目标线性表指针
+ * pos     目标插入位置
+ * node    目标节点
+ */
+Status append_after_list_node( ArrayList *list, ArrayListNode **pos, ArrayListNode *node );
+
+/*
+ * 删除顺序线性表表头节点，并以node返回
+ *
+ * list    目标线性表指针
+ * node    返回的表头节点
+ */
+Status delete_list_head( ArrayList *list, ArrayListNode **node );
+
+/*
+ * 删除顺序线性表表尾节点，并以node返回
+ *
+ * list    目标线性表指针
+ * node    返回的表尾节点
+ */
+Status remove_list_tail( ArrayList *list, ArrayListNode **node );
 
 /*
  * 删除一个节点
  *
- *   返回删除节点数据
- *   若未找到待删除节点，返回空
- *
  * list   目标线性表指针
- * index  待删除节点下标，定义域为[0, list_size( list ) - 1]
+ * pos    待删除节点位置
  */
-void *list_delete( ArrayList *list, long index );
+Status delete_list_node( ArrayList *list, ArrayListNode **pos );
 
 /*
- * 链表遍历
+ * 返回顺序线性表的表头位置
+ *
+ * list   目标线性表指针
+ */
+ArrayListNode *get_list_head( ArrayList *list );
+
+/*
+ * 返回顺序线性表的表尾位置
+ *
+ * list   目标线性表指针
+ */
+ArrayListNode *get_list_tail( ArrayList *list );
+
+/*
+ * 设置节点内容
+ *
+ * node   目标节点
+ * data   数据内容
+ */
+Status set_list_node_content( ArrayListNode *pos, void *data );
+
+/*
+ * 获取节点内容
+ *
+ * node   目标节点
+ */
+void *get_list_node_content( ArrayListNode *pos );
+
+/*
+ * 顺序线性表遍历
  *
  * list    目标线性表指针
  * handle  函数句柄，对线性表中的每一个元素做相应的操作
  */
-void list_traverse( ArrayList *list, void (*handle)(void *) );
+Status list_traverse( ArrayList *list, Status (*handle)(void *) );
 
 #endif
