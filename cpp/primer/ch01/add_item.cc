b72b03ab0e76b4d4b3886e1ae32ac76759b5dd91
David Ding <chengjie.ding@gmail.com>
2013-09-10 23:36:46 +0800

    refactoring add_item.cc and remove compile warning in makefile

--- cpp/primer/ch01/add_item.cc
+++ cpp/primer/ch01/add_item.cc
@@ -6,7 +6,13 @@ int main( void )
   Sales_item item1, item2;
 
   std::cin >> item1 >> item2;
-  std::cout << item1 + item2 << std::endl;
+  if( item1.isbn() == item2.isbn() ) {
+    std::cout << item1 + item2 << std::endl;
+    return 0;
+  } else {
+    std::cerr << "Data must refer to same ISBN" << std::endl;
+    return -1;
+  }
 
   return 0;
 }
