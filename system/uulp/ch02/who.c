b35f5c992c066c782263b57e21eb57efe9e01b4c
David Ding <chengjie.ding@gmail.com>
2013-09-14 14:54:35 +0800

    add file handler system call

---
+++ system/uulp/ch02/who.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <utmp.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define SHOWHOST
+
+void show_info( struct utmp * );
+
+int main( void )
+{
+  struct utmp current_record;
+  int utmpfd;
+  int reclen = sizeof( current_record );
+
+  if( ( utmpfd = open( UTMP_FILE, O_RDONLY ) ) == -1 ) {
+    perror( UTMP_FILE );
+    exit( 1 );
+  }
+
+  while( read( utmpfd, &current_record, reclen ) == reclen ) {
+    show_info( &current_record );
+  }
+
+  close( utmpfd );
+  return 0;
+}
+
+void show_info( struct utmp *utbufp )
+{
+  printf( "%-8.8s", utbufp->ut_name );
+  printf( " " );
+  printf( "%-8.8s", utbufp->ut_line );
+  printf( " " );
+  printf( "%10ld", utbufp->ut_time );
+  printf( " " );
+#ifdef SHOWHOST
+  printf( "( %s )", utbufp->ut_host );
+#endif
+  printf( "\n" );
+}
