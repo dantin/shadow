e7e7ad4ae3ca461850cb455c68f1e3bdc1d4f5ec
David Ding <chengjie.ding@gmail.com>
2013-09-26 13:29:33 +0800

    add signal processing demo

---
+++ system/uulp/ch06/sigdemo.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <signal.h>
+
+void f( int );
+
+int main( void )
+{
+  int i;
+
+  signal( SIGINT, f );
+  for( i = 0; i < 5; i++ ) {
+    printf( "hello\n" );
+    sleep( 1 );
+  }
+
+  return 0;
+}
+
+void f( int signum )
+{
+  printf( "OUCH!\n" );
+}
