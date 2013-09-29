7bfe180778237b2fbd7105ec953dc3502d7163c7
David Ding <chengjie.ding@gmail.com>
2013-09-29 10:13:38 +0800

    add demo to bounce a message

---
+++ system/uulp/ch07/bounce.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <curses.h>
+
+#define LEFTEDGE  10
+#define RIGHTEDGE 30
+#define ROW       10
+
+int main( void )
+{
+  char *message = "Hello";
+  char *blank =   "     ";
+  int dir = +1;
+  int pos = LEFTEDGE;
+
+  initscr();
+  clear();
+
+  while( 1 ) {
+    move( ROW, pos );
+    addstr( message );
+    move( LINES - 1, COLS - 1 );
+    refresh();
+    sleep( 1 );
+    move( ROW, pos );
+    addstr( blank );
+    pos += dir;
+    if( pos >= RIGHTEDGE ) {
+      dir = -1;
+    }
+    if( pos <= LEFTEDGE ) {
+      dir = +1;
+    }
+  }
+
+  endwin();
+
+  return 0;
+}
