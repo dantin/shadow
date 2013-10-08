6d9d33ac5d4fb248b4be59aa555b5cdd107dae9d
David Ding <chengjie.ding@gmail.com>
2013-10-08 16:51:18 +0800

    add sigaction api

---
+++ system/uulp/ch07/sigactdemo.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <signal.h>
+
+#define INPUTLEN 100
+
+void inthandler( int );
+
+int main( void )
+{
+  struct sigaction newhandler;
+  sigset_t blocked;
+  char x[ INPUTLEN ];
+
+  newhandler.sa_handler = inthandler;
+  newhandler.sa_flags = SA_RESETHAND | SA_RESTART;
+  sigemptyset( &blocked );
+  sigaddset( &blocked, SIGQUIT );
+  newhandler.sa_mask = blocked;
+
+  if( sigaction( SIGINT, &newhandler, NULL ) == -1 ) {
+    perror( "sigaction" );
+  } else {
+    while( 1 ) {
+      fgets( x, INPUTLEN, stdin );
+      printf( "input: %s", x );
+    }
+  }
+}
+
+void inthandler( int s )
+{
+  printf( "Called with signal %d\n", s );
+  sleep( s );
+  printf( "done handling signal %d\n", s );
+}
