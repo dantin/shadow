b5a248d9c6f85efc25b9c4b1459de667585bb258
David Ding <chengjie.ding@gmail.com>
2013-10-11 17:03:36 +0800

    show how to run a program

---
+++ system/uulp/ch08/exec.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main( void )
+{
+  char *arglist[3];
+
+  arglist[0] = "ls";
+  arglist[1] = "-l";
+  arglist[2] = 0;
+
+  printf( "*** About to exec ls -l\n" );
+  execvp( "ls", arglist );
+  printf( "*** ls is done. bye\n" );
+
+  return 0;
+}
