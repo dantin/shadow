5eb53264655a51fe0bca5a3e16ec09f024e8a673
David Ding <chengjie.ding@gmail.com>
2013-10-17 15:20:09 +0800

    read and print the environment

---
+++ system/uulp/ch09/showenv.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+extern char **environ;
+
+int main( void )
+{
+  int i;
+
+  for( i = 0; environ[i]; i++ ) {
+    printf( "%s\n", environ[i] );
+  }
+
+  return 0;
+}
