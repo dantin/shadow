5011d2531838bb7589a44c1856ea95a72d1d97c0
David Ding <chengjie.ding@gmail.com>
2013-11-05 16:40:15 +0800

    get mapping item via get

---
+++ python/bp/ch04/phone_db.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+people = {
+    'Alice' : {
+        'phone': '2341',
+        'addr' : 'Foo drive 23'
+    },
+    'Beth'  : {
+        'phone': '9102',
+        'addr' : 'Bar street 42'
+    },
+    'Cecil' : {
+        'phone': '3158',
+        'addr' : 'Baz avenue 90'
+    }
+}
+
+labels = {
+    'phone': 'phone number',
+    'addr' : 'address'
+}
+
+name = raw_input( 'Name: ' )
+
+request = raw_input( 'phone number(p) or address(a)? ' )
+
+key = request
+if request == 'p': key = 'phone'
+if request == 'a': key = 'addr'
+
+person = people.get( name, {} )
+label = labels.get( key, key )
+result = person.get( key, 'not avaiable' )
+
+print "%s's %s is %s." % \
+    ( name, label, result )
