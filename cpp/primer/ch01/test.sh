9eef31cbf4c257893179aef3afc97e885763d001
David Ding <chengjie.ding@gmail.com>
2013-11-12 11:29:15 +0800

    refactoring test.sh

--- cpp/primer/ch01/test.sh
+++ cpp/primer/ch01/test.sh
@@ -2,12 +2,12 @@
 
 EXE_DIR=.
 
-$EXE_DIR/main_only
-$EXE_DIR/add < data/add
-$EXE_DIR/while_count
-$EXE_DIR/for_count
-$EXE_DIR/sum < data/sum
-$EXE_DIR/occurs < data/occurs
-$EXE_DIR/item_io < data/item_io
-$EXE_DIR/add_item < data/add_item
-$EXE_DIR/avg_price < data/book_sales
+${EXE_DIR}/main_only
+${EXE_DIR}/add < data/add
+${EXE_DIR}/while_count
+${EXE_DIR}/for_count
+${EXE_DIR}/sum < data/sum
+${EXE_DIR}/occurs < data/occurs
+${EXE_DIR}/item_io < data/item_io
+${EXE_DIR}/add_item < data/add_item
+${EXE_DIR}/avg_price < data/book_sales
