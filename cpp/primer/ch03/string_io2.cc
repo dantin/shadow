67e0c05ef441832e87658cee25b6031499f13eab
David Ding <chengjie.ding@gmail.com>
2013-11-22 16:02:34 +0800

    add string input

---
+++ cpp/primer/ch03/string_io2.cc
@@ -0,0 +1,17 @@
+#include <string>
+using std::string;
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main( void )
+{
+  string s1, s2;
+
+  cin >> s1 >> s2;
+  cout << s1 << s2 << endl;
+
+  return 0;
+}
--- cpp/primer/ch03/test.sh
+++ cpp/primer/ch03/test.sh
@@ -4,4 +4,5 @@ EXE_DIR=.
 
 ${EXE_DIR}/add_using < data/add
 ${EXE_DIR}/string_io < data/string_io
+${EXE_DIR}/string_io2 < data/string_io
 
