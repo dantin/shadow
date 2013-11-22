a78719592c2548be8b79075f32ae6b77bd4669d9
David Ding <chengjie.ding@gmail.com>
2013-11-22 15:56:16 +0800

    add string io

---
+++ cpp/primer/ch03/string_io.cc
@@ -0,0 +1,17 @@
+#include <string>
+using std::string;
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main( void )
+{
+  string s;
+
+  cin >> s;
+  cout << s << endl;
+
+  return 0;
+}
--- cpp/primer/ch03/test.sh
+++ cpp/primer/ch03/test.sh
@@ -3,4 +3,5 @@
 EXE_DIR=.
 
 ${EXE_DIR}/add_using < data/add
+${EXE_DIR}/string_io < data/string_io
 
